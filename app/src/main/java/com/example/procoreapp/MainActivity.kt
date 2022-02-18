a2e858a9e306dd7cc63a6cdd451d3c3c1ef12891
hemaladani <hkdeveloper2711@gmail.com>
2022-02-18 16:00:20 -0500

    Added Search functionality

--- app/src/main/java/com/example/procoreapp/MainActivity.kt
+++ app/src/main/java/com/example/procoreapp/MainActivity.kt
@@ -2,22 +2,37 @@ package com.example.procoreapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModel
+import android.util.Log
+import android.widget.EditText
+import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
     lateinit var mainViewModel: MainViewModel
+    lateinit var edtSearch:EditText
+    var pokemonDataAdapter:PokemonDataAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recyclerView = findViewById(R.id.recyclerview)
+        edtSearch=findViewById(R.id.edt_search)
         mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         mainViewModel.fetchData().observe(this,{pokemons->
-
+                Log.d("",""+pokemons.data)
+                pokemonDataAdapter = PokemonDataAdapter()
+                pokemonDataAdapter?.updateData(pokemons.data?: emptyList())
+                recyclerView.adapter = pokemonDataAdapter
+                recyclerView.layoutManager=GridLayoutManager(this,3)
             }
         )
+        edtSearch.doOnTextChanged { text, start, before, count ->
+            pokemonDataAdapter?.searchResults(text.toString())
+
+        }
+
 
     }
 }
--- app/src/main/java/com/example/procoreapp/PokemonDataAdapter.kt
+++ app/src/main/java/com/example/procoreapp/PokemonDataAdapter.kt
@@ -5,10 +5,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.procoreapp.modal.Datum
 import com.example.procoreapp.modal.Pokemon
 import com.squareup.picasso.Picasso
 
-class PokemonDataAdapter(val items:Pokemon): RecyclerView.Adapter<PokemonDataAdapter.PokemonViewHolder>() {
+class PokemonDataAdapter(): RecyclerView.Adapter<PokemonDataAdapter.PokemonViewHolder>() {
+    var data:List<Datum>? = null
     inner class PokemonViewHolder(itemview: View): RecyclerView.ViewHolder(itemview) {
         val items:ImageView
         init {
@@ -22,9 +24,25 @@ class PokemonDataAdapter(val items:Pokemon): RecyclerView.Adapter<PokemonDataAda
     }
 
     override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
-        val data = items.data?.get(position) ?: return
+        val data = data?.get(position) ?: return
         Picasso.get().load(data.images?.small).into(holder.items)
     }
 
-    override fun getItemCount() = items.data?.size?:0
+    override fun getItemCount() = data?.size?:0
+    public fun updateData(items:List<Datum>){
+        //data = items
+        notifyDataSetChanged()
+    }
+    public fun searchResults(search:String){
+        val searchResults = mutableListOf<Datum>()
+        data?.let{
+            for(i in it){
+                if(i.name?.contains(search)==true){
+                    searchResults.add(i)
+                }
+            }
+            updateData(searchResults)
+        }
+
+    }
 }
